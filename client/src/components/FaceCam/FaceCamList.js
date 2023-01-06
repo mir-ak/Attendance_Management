54f632068cc727ed2a19eaf3a969a7323ed55c63
mir-ak <mirakcr7@gmail.com>
2023-01-06 23:42:40 +0100

    update display data and add CircularProgressWithLabel (loading)

--- client/src/components/FaceCam/FaceCamList.js
+++ client/src/components/FaceCam/FaceCamList.js
@@ -13,6 +13,51 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Stack from "@mui/material/Stack";
+import PropTypes from "prop-types";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
+//mport MaterialIcon from "material-icons-react";
+import Button from "@mui/material/Button";
+
+const CircularProgressWithLabel = (props) => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        <Typography variant="caption" component="div" color="text.secondary">
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+CircularProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate variant.
+   * Value between 0 and 100.
+   * @default 0
+   */
+  value: PropTypes.number.isRequired,
+};
 
 export default class ProjectList extends Component {
   state = {
@@ -20,6 +65,7 @@ export default class ProjectList extends Component {
     faceCam: [],
     currentPage: 1,
     elementsPerPage: 10,
+    progress: 10,
     today: new Date(),
     currentDate: new Date().toLocaleDateString(),
     currentTime: null,
@@ -79,6 +125,16 @@ export default class ProjectList extends Component {
         });
       });
     });
+    const timer = setInterval(() => {
+      this.setState((prevState) => {
+        const prevProgress = prevState.progress;
+        const progress = prevProgress >= 100 ? 130 : prevProgress + 22.5;
+        return { progress };
+      });
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
   }
 
   render() {
@@ -158,43 +214,59 @@ export default class ProjectList extends Component {
               />
             </li>
           </LocalizationProvider>
+          <Box sx={{ flexGrow: 0.98 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Tooltip title="Add Products">
+              <Stack spacing={2} direction="row">
+                <Button variant="outlined">Outlined</Button>
+              </Stack>
+            </Tooltip>
+          </Box>
         </ul>
-        <div className="projects">
-          {filterDate.length === 0 && filterTime.length === 0 ? (
-            <Stack sx={{ width: "100%" }} spacing={50} paddingTop={5}>
-              <Alert severity="warning">
-                <AlertTitle>Warning</AlertTitle>
-                No one is present on this date {currentDate}{" "}
-                {currentTime !== null ? "or this time " + currentTime : null}{" "}
-                <strong>check it out!</strong>
-              </Alert>
-            </Stack>
-          ) : faceCam.length > 0 && currentTime !== null ? (
-            filterTime
-              .slice(indexOfFirstElement, indexOfLastElement)
-              .map((item) => {
-                return <FaceCam key={item.id} item={item} />;
-              })
-          ) : (
-            filterDate
-              .slice(indexOfFirstElement, indexOfLastElement)
-              .map((item) => {
-                return <FaceCam key={item.id} item={item} />;
-              })
-          )}
-        </div>
-        <div className="pagination">
-          <Pagination
-            color="primary"
-            defaultPage={1}
-            count={
-              currentTime != null
-                ? Math.ceil(filterTime.length / elementsPerPage)
-                : Math.ceil(filterDate.length / elementsPerPage)
-            }
-            onChange={(event, page) => this.setState({ currentPage: page })}
-          />
-        </div>
+        {this.state.progress <= 100 ? (
+          <CircularProgressWithLabel value={this.state.progress} />
+        ) : (
+          <>
+            <div className="projects">
+              {filterDate.length === 0 && filterTime.length === 0 ? (
+                <Stack sx={{ width: "100%" }} spacing={50} paddingTop={5}>
+                  <Alert severity="warning">
+                    <AlertTitle>Warning</AlertTitle>
+                    No one is present on this date {currentDate}{" "}
+                    {currentTime !== null
+                      ? "or this time " + currentTime
+                      : null}{" "}
+                    <strong>check it out!</strong>
+                  </Alert>
+                </Stack>
+              ) : faceCam.length > 0 && currentTime !== null ? (
+                filterTime
+                  .slice(indexOfFirstElement, indexOfLastElement)
+                  .map((item) => {
+                    return <FaceCam key={item.id} item={item} />;
+                  })
+              ) : (
+                filterDate
+                  .slice(indexOfFirstElement, indexOfLastElement)
+                  .map((item) => {
+                    return <FaceCam key={item.id} item={item} />;
+                  })
+              )}
+            </div>
+            <div className="pagination">
+              <Pagination
+                color="primary"
+                defaultPage={1}
+                count={
+                  currentTime != null
+                    ? Math.ceil(filterTime.length / elementsPerPage)
+                    : Math.ceil(filterDate.length / elementsPerPage)
+                }
+                onChange={(event, page) => this.setState({ currentPage: page })}
+              />
+            </div>
+          </>
+        )}
       </div>
     );
   }
