21617e1efcab8bcf8f7464ce311ae6417f0e339b
mir-ak <mirakcr7@gmail.com>
2023-01-12 00:29:23 +0100

    update notFoundpage

--- client/src/page/NotFound.js
+++ client/src/page/NotFound.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../styles/pages/_notFound.scss';
+import React from "react";
+import "../styles/pages/_notFound.scss";
 function NotFound() {
   return (
     <div className="notFound">
-      <h3>404 <br />Page not fond </h3>
+      <h3>
+        404 <br />
+        Page not fond{" "}
+      </h3>
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
