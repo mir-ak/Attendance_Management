0cfe9c598b5e95b7163d2a3a710d152f4aea99f3
mir-ak <mirakcr7@gmail.com>
2023-01-15 22:00:19 +0100

    update firebase config

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -4,14 +4,15 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAy7BuwZg5osnug1sM104vr-tuY9QCb0pw",
-  authDomain: "fasecam-44231.firebaseapp.com",
+  apiKey: "AIzaSyB8UyAVJ9GlHwav0UIcjYzKKpUHQFJVqa0",
+  authDomain: "attendancemanagement-7bf42.firebaseapp.com",
   databaseURL:
-    "https://fasecam-44231-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fasecam-44231",
-  storageBucket: "fasecam-44231.appspot.com",
-  messagingSenderId: "603911072055",
-  appId: "1:603911072055:web:c558f42d529e5d1bb912b2",
+    "https://attendancemanagement-7bf42-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "attendancemanagement-7bf42",
+  storageBucket: "attendancemanagement-7bf42.appspot.com",
+  messagingSenderId: "492048061748",
+  appId: "1:492048061748:web:b35a043c87ad39ba1b0161",
+  measurementId: "G-ZCE5PQ6V10",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
