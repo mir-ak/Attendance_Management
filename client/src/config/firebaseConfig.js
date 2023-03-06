74c0487a72fef899a27cc391c867b3ab3467bdb6
mir-ak <mirakcr7@gmail.com>
2023-03-06 21:06:48 +0100

    add variable env

--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -2,17 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+require("dotenv").config();
 
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyB8UyAVJ9GlHwav0UIcjYzKKpUHQFJVqa0",
-  authDomain: "attendancemanagement-7bf42.firebaseapp.com",
-  databaseURL:
-    "https://attendancemanagement-7bf42-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "attendancemanagement-7bf42",
-  storageBucket: "attendancemanagement-7bf42.appspot.com",
-  messagingSenderId: "492048061748",
-  appId: "1:492048061748:web:b35a043c87ad39ba1b0161",
-  measurementId: "G-ZCE5PQ6V10",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
